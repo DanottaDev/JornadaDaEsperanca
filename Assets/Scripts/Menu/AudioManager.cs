986e80273d9110b4b63d7e4c38f8a5c17ff01218
Erick Santos Brito <brito.santos@aluno.ifsp.edu.br>
2024-06-28 02:00:40 -0300

    Controlador de áudio do menu

--- Assets/Scripts/Menu/AudioManager.cs
+++ Assets/Scripts/Menu/AudioManager.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField]public AudioSource musicSource;
+    private bool estadoSom = true;
+    [SerializeField] private AudioSource fundoMusical;
+    [SerializeField] private Sprite somLigadoSprite;
+    [SerializeField] private Sprite somDesligadoSprite;
+    [SerializeField] private Image muteImage;
+    public void LigarDesligarSom()
+    {
+        estadoSom = !estadoSom;
+        fundoMusical.enabled = estadoSom;
+        
+        if (estadoSom)
+        {
+            muteImage.sprite = somLigadoSprite;
+        }
+        else
+        {
+            muteImage.sprite = somDesligadoSprite;
+        }
+    }
 
     public void VolumeMusical (float value)
     {
-        musicSource.volume = value;
+        fundoMusical.volume = value;
     }
 }
