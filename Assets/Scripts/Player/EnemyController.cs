ebb421e6c5e60aa906af6f961e6919ba425ecfae
Erick Santos Brito <brito.santos@aluno.ifsp.edu.br>
2024-07-11 01:26:03 -0300

    Distribuindo inimigos pelo cenário

--- Assets/Scripts/Player/EnemyController.cs
+++ Assets/Scripts/Player/EnemyController.cs
@@ -4,16 +4,55 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public int maxHealth = 3;
+    public float speed = 2f;
+    public Transform[] waypoints;
+    
     private int currentHealth;
+    private int currentWaypointIndex = 0;
+    private bool isAttacking = false;
+    private bool isDying = false;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
-    private bool isDying = false;
+    private Vector3 lastPosition;
 
     private void Start()
     {
         currentHealth = maxHealth;
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        lastPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (!isAttacking && !isDying)
+        {
+            Patrol();
+        }
+    }
+
+    private void Patrol()
+    {
+        Transform targetWaypoint = waypoints[currentWaypointIndex];
+        Vector2 direction = (targetWaypoint.position - transform.position).normalized;
+        transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, targetWaypoint.position) < 0.1f)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        }
+
+        // Flip the sprite based on the movement direction
+        if (transform.position.x > lastPosition.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (transform.position.x < lastPosition.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        lastPosition = transform.position;
     }
 
     public void TakeDamage(int damage)
