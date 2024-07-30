710a459b5b37ca342e0c2a746e9a3ccd863eb183
Erick Santos Brito <brito.santos@aluno.ifsp.edu.br>
2024-07-30 04:32:54 -0300

    [Correção] Corrige script que chama a próxima cena.

--- Assets/Scripts/OpeningCutscene.cs
+++ Assets/Scripts/OpeningCutscene.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class OpeningCutscene : MonoBehaviour
 {
     void OnEnable()
     {
-        SceneManager.LoadScene("Level01", LoadSceneMode.Single);
+        // Carrega a próxima cena no build index
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1, LoadSceneMode.Single);
     }
-
 }
